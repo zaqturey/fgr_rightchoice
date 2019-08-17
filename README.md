581d4d72d8cd36944ee429df2183e8349bfc3421
Zaq Turey <zaqturey@gmail.com>
2019-08-17 19:20:02 +0100

    Implemented '_answerQuestion', setState, 'MaterialApp' widget that implements an 'appBar' and 'body'

---
+++ README.md
@@ -0,0 +1,25 @@
+# rigthtchoice
+
+A new Flutter application for multi choice answers
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## Commits History (newest on the top)
+
+>> Implemented '_answerQuestion', setState, 'MaterialApp' widget that implements an 'appBar' and 'body'
+- main.dart >> Implemented a void function i.e. '_answerQuestion' that performs 'setState()' which is an inbuilt function.
+- main.dart >> 'setState()' function increments the value of '_questionIndex'
+- main.dart >> Added List of Strings i.e. questions
+- main.dart >> _MyAppState returns a 'MaterialApp' that implements an 'appBar' and 'body'
+- main.dart >> Body returns a 'Column' that returns a 'children' that returns a 'Text' and three 'RaisedButton' widgets
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp>{
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      if(_questionIndex == 2){
+        _questionIndex = -1;
+      }
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // List of Strings(Questions)
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite dish?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Right Choice'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Option 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Option 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Option 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
